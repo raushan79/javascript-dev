cd36011a798c6cdaae0e3fb8f3645ed34ac4d2af
RAUSHAN KUMAR <raushansft@gmail.com>
2024-11-01 07:12:29 +0000

    exploring array in javascript

--- javascript_basic/js_array.js
+++ javascript_basic/js_array.js
@@ -1,14 +1,23 @@
-console.log(`Learning JS Array`)
+console.log(`Learning Array in Jascript`)
 
 // create a array
-let arr = Array.of(5) // create a array with 1 element
+const createArray = () => {
+    let arr = Array.of(5) // create a array with 1 element
+    
+    arr = new Array(5); // create a new array with 5 elements
+    arr[0] = 10
+    
+    // console.log(arr.length)
+    
+    let numArray = [1, 2, 3, 4, 5]
+    numArray.splice(3,2)
+    console.log(numArray)
+    console.log(numArray.indexOf(10))
+}
 
-arr = new Array(5); // create a new array with 5 elements
-arr[0] = 10
+/**
+ * Array build in method
+ */
 
-// console.log(arr.length)
 
-let numArray = [1, 2, 3, 4, 5]
-numArray.splice(3,2)
-console.log(numArray)
-console.log(numArray.indexOf(10))
+// executable function
